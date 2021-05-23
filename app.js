b9cc53db878439cfa5b419ba2f436b1ace62bcfc
Victor Moraes <vitao.moraes@gmail.com>
2021-05-23 11:30:00 -0300

    trying to deploy heroku

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req,res){
     res.sendFile('index.html');
 })
 
-const PORT = 80;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, function(req,res){
     console.log('Listening on port ' + PORT + '...');
