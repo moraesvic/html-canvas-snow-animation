897e3fd5c165faedfe5e7b670a6ea5dfb6033666
Victor Moraes <vitao.moraes@gmail.com>
2021-05-23 11:23:10 -0300

    changed to port 8080

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function(req,res){
     res.sendFile('index.html');
 })
 
-app.listen(3000, function(req,res){
+app.listen(8080, function(req,res){
     console.log('listening on port 3000');
 });
